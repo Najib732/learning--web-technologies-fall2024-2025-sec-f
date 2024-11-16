0387ab8a4878f53559e1cc2a28c3b15e6fe4aaa9
Najib Mahfuj <22-48248-2@student.aiub.edu>
2024-11-16 06:24:27 +0600

    Update

--- LabWork/LAB3/Task7.php
+++ LabWork/LAB3/Task7.php
@@ -2,29 +2,28 @@
 
 
 for ($i = 0; $i < 3; $i++) {
-    for ($j = 0; $j <= $i; $j++) {
-        echo "*";
-    }
-    echo "<br>";
+  for ($j = 0; $j <= $i; $j++) {
+    echo "*";
+  }
+  echo "<br>";
 }
 
 echo "<br><br>";
 
 
 for ($i = 3; $i > 0; $i--) {
-    for ($j = 0; $j < $i; $j++) {
-        echo $j + 1;
-    }
-    echo "<br>";
+  for ($j = 0; $j < $i; $j++) {
+    echo $j + 1;
+  }
+  echo "<br>";
 }
 
 echo "<br><br>";
 
 for ($i = 0; $i < 3; $i++) {
-    $startChar = 'A';
-    for ($j = 0; $j <= $i; $j++) {
-        echo chr($startChar + $j);
-    }
-    echo "<br>";
+  $startChar = 'A';
+  for ($j = 0; $j <= $i; $j++) {
+    echo chr($startChar + $j);
+  }
+  echo "<br>";
 }
-?>
