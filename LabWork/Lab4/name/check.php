120f5d54ffe9aea504e6ecd3002f4f3bd64b3b8a
Najib Mahfuj <22-48248-2@student.aiub.edu>
2024-11-19 00:05:13 +0600

    push name

---
+++ LabWork/Lab4/name/check.php
@@ -0,0 +1,56 @@
+<?php
+
+session_start();
+
+if (isset($_GET['submit'])) {
+
+    $username = $_REQUEST['name'];
+    $count = strlen($username);
+    $letter = substr($username, 0,1);
+    $firstLetter;
+    $number=0;
+
+    $array  =[
+        "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+",
+        "[", "{", "]", "}", ";", ":", "\\", "<", ">", ".", "/", "?", "|", 
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+    ];
+    $array2 = [
+        "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+",
+        "[", "{", "]", "}", ";", ":", "\\", "<", ">", ".", "/", "?", "|", 
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    ];
+    
+
+    /*
+    for ($i = 0; $i < count($array); $i++) {
+        if ($letter == $array[$i]) {
+            $firstLetter = true;
+            echo "$letter";
+        }
+    }
+    */
+    for ($j = 0; $j < $count; $j++){
+    for ($i = 0; $i < count($array2); $i++) {
+        if ($username[$j] == $array2[$i]) {
+      
+            $number=$number+1;
+        }
+       
+    }
+}
+
+echo $number;
+
+    if ($username==null||$count>=2||in_array($letter, $array)==true||$number==$count) {
+        echo "Fill the textbox";
+    } 
+    else {
+
+        echo  "{$count}";
+    }
+    
+    
+}
