a6d374147b1c4bbeab1f3aa86216f54a9792952b
Najib Mahfuj <22-48248-2@student.aiub.edu>
2024-11-21 04:05:06 +0600

    re-push Lab3

---
+++ LabWork/LAB3/class.php
@@ -0,0 +1,18 @@
+<?php
+ 
+ $value="6";
+ $Value="6";
+
+    if($value==$Value)
+    {
+       echo "will not check the data type";
+    }
+    else if($value==$Value)
+    {
+       echo  "will check the data type.if the data type will match then it will compare otherwise it will not.";
+    }
+    else{
+        echo "najib{8}";
+    }
+
+?> 
